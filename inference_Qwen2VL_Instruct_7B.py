660a5a9c6fe64e0f2086d34e53829f093a908aaf
Adrian-Cespedes <adrian.cespedes@utec.edu.pe>
2025-01-15 22:21:01 -0500

    Added functional Qwen2VL script

--- inference_Qwen2VL_Instruct_7B.py
+++ inference_Qwen2VL_Instruct_7B.py
@@ -19,12 +19,12 @@ model = Qwen2VLForConditionalGeneration.from_pretrained(
 )
 
 # default processer
-# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
 
 # The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
-min_pixels = 256*28*28
-max_pixels = 1280*28*28
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
 # Function to make inference with the model
 def inference_with_images(image_folder, question):
