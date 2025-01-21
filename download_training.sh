f54189b7c9b3b5fc27ca967191eeeea537cc16f0
Adrian-Cespedes <adrian.cespedes@utec.edu.pe>
2025-01-21 12:07:09 -0500

    Changed download_training.sh

--- download_training.sh
+++ download_training.sh
@@ -1,7 +1,7 @@
 # Scenery Train dataset
-gdown https://drive.google.com/drive/folders/1GiwWGfrM8pO27CYLu_9Uwtdcz0JoqHr7 -O scenery/ --folder
+gdown https://drive.google.com/drive/folders/1GiwWGfrM8pO27CYLu_9Uwtdcz0JoqHr7 -O training/ --folder
 
 # Action Train dataset
-gdown https://drive.google.com/drive/folders/1QQqBrR3uGDC05Zc11zMeui6Zzl7RvFZg -O action/ --folder
+gdown https://drive.google.com/drive/folders/1QQqBrR3uGDC05Zc11zMeui6Zzl7RvFZg -O training/ --folder
 
 huggingface-cli download unsloth/Qwen2-VL-7B-Instruct
