509f813c855ff22960656ace4bd002f44bb48642
Adrian-Cespedes <adrian.cespedes@utec.edu.pe>
2025-01-21 11:57:44 -0500

    Added training files download script

---
+++ download_training.sh
@@ -0,0 +1,7 @@
+# Scenery Train dataset
+gdown https://drive.google.com/drive/folders/1GiwWGfrM8pO27CYLu_9Uwtdcz0JoqHr7 -O scenery/ --folder
+
+# Action Train dataset
+gdown https://drive.google.com/drive/folders/1QQqBrR3uGDC05Zc11zMeui6Zzl7RvFZg -O action/ --folder
+
+huggingface-cli download unsloth/Qwen2-VL-7B-Instruct
